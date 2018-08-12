d7b860a1feaa563c4aaffeb93977706ca0f04045
Athos <athosvankralingen@hotmail.com>
2018-08-13 00:36:17 +0200

    Added missing docs

--- CalculatorInternals/MultiplicationOperator.cs
+++ CalculatorInternals/MultiplicationOperator.cs
@@ -7,11 +7,19 @@ namespace CalculatorInternals
     /// </summary>
     public class MultiplicationOperator : BinaryOperator
     {
+        /// <summary>
+        /// Evaluates (folds) the expression to a single value, evaluating all its operands
+        /// </summary>
+        /// <returns>The result of the expression</returns>
         public override decimal Evaluate()
         {
             return m_FirstOperand.Evaluate() * m_SecondOperand.Evaluate();
         }
 
+        /// <summary>
+        /// Converts the multiplication expression to a string representation
+        /// </summary>
+        /// <returns>The stringified representation of the multiplication expression</returns>
         public override string ToString()
         {
             StringBuilder stringBuilder = new StringBuilder(m_FirstOperand.ToString() + " × ");
